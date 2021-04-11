6383bab51cd7344e05224a101e99afad9b0cde4b
Caitlyn Carney <caitlyn.r.carney@gmail.com>
2021-04-11 18:01:38 -0500

    Exploration Functions

--- Workbooks/explore.py
+++ Workbooks/explore.py
@@ -4,17 +4,12 @@ from scipy import stats
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import matplotlib.axes as ax
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-
-from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
-from statsmodels.formula.api import ols
-from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.feature_selection import f_regression 
 
 import wrangle
 
