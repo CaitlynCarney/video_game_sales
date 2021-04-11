bdef4414df4431bc36b8df730d63e6414da820fb
Caitlyn Carney <caitlyn.r.carney@gmail.com>
2021-04-11 13:31:04 -0500

    Modeling Functions

---
+++ Workbooks/model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from math import sqrt
+import seaborn as sns
+
+# modeling methods
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from statsmodels.formula.api import ols
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_selection import f_regression 
+
+import warnings
+warnings.filterwarnings("ignore")
+import acquire
+import prepare
+import explore
+import evaluate
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
